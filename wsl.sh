36fe5ee198a2a779a4a8f66346b4ea84a09d00dc
andredw13L <102394462+andredw13L@users.noreply.github.com>
2023-02-11 23:09:13 -0300

    Create wsl.sh

---
+++ wsl.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+sudo apt install -y php7.4 php7.4-xml php7.4-mysql php7.4-gd php7.4-curl php7.4-pdo php7.4-mbstring php7.4-sqlite3 php7.4-zip php7.4-pgsql php7.4-opcache php7.4-json php7.4-intl
+
+sudo apt install -y git wget curl man-db nano micro htop fd-find ranger cmatrix neofetch bat  python3-pip openjdk-17-jdk openjdk-17-doc maven
+
+sudo apt install -y clang gcc tldr sqlite3 sqlite3-doc docker.io lolcat
+
+tldr update
+
+
+xdg-user-dirs-update
