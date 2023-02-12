33569496ffa7c555fd6ed5d948e38bb4ddef65bc
andredw13L <102394462+andredw13L@users.noreply.github.com>
2023-02-11 23:11:50 -0300

    Update wsl.sh

--- wsl.sh
+++ wsl.sh
@@ -4,7 +4,7 @@ sudo apt install -y php7.4 php7.4-xml php7.4-mysql php7.4-gd php7.4-curl php7.4-
 
 sudo apt install -y git wget curl man-db nano micro htop fd-find ranger cmatrix neofetch bat  python3-pip openjdk-17-jdk openjdk-17-doc maven
 
-sudo apt install -y clang gcc tldr sqlite3 sqlite3-doc docker.io lolcat
+sudo apt install -y clang gcc tldr sqlite3 sqlite3-doc docker.io lolcat bash-completion
 
 tldr update
 
